96c46106998968d7d2acf955a1a1b0d2530c78b7
Nathan Samijo <0961962@hr.nl>
2020-03-30 22:14:16 +0200

    Updated the README and Added the Boek class

---
+++ Boek.py
@@ -0,0 +1,9 @@
+class Boek:
+    title = ''
+    author = ''
+    year = 0000
+    
+    id = 0
+
+    def __init__(self, ):
+        
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Analyse-3-Eind-Opdracht
+
+Dit is once eind opdracht.
+
+Contributors:
+- Jonas de Boer
+- Nathan Samijo
