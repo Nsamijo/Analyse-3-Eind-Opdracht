c980bbb111076007436ce4c8bdab9864e3bdc5ef
PhantomNathan <nsamijo@gmail.com>
2020-05-14 16:13:41 +0200

    Added comments to the  PLS document

--- BookManager/PLS.py
+++ BookManager/PLS.py
@@ -1,16 +1,26 @@
+#import os used to clear the terminal and time to make the thread sleep
 import os, time
+#self made "modules" to separate it a bit
 import BookAdmin, UserClasses, helper
+#this lambda is used to clear the terminal #science
 clear = lambda : os.system('cls')
 
 class PLS:
+    #variables which are very much needed
+    #admin to know if it's a customer or not
     admin = None
+    #logged to hold the logged in libarian
     logged = None
+    #librarians to hold the librarians
     librarians = None
+    #customers to hold all the customers
     customers = None
 
     def __init__(self):
+        #initiate the files with the data || read the data in
         self.librarians = UserClasses.Librarians()
         self.customers = UserClasses.Customers()
+
     #title
     def printTitle(self):
         print('Public Libary System\n')
@@ -39,6 +49,7 @@ class PLS:
                 clear()
                 return False
 
+            #find out if the person is a customer or a librarian
             while temp not in ['1', '2']:
                 temp = welcomeScreen(False)
                 #check if for exit
@@ -102,6 +113,7 @@ class PLS:
 
         #function that will be able to make changes to customers
         def customerEditing():
+            #check what the librarian wants to do
             loggedIn()
             print('What do you wish to do?\n[1] Register customer\n[2] Remove customer\n[3] Edit customer\n[4] Back\n')
             choice = input('>>> ')
@@ -110,39 +122,53 @@ class PLS:
                 print('What do you wish to do?\n[1] Register customer\n[2] Remove customer\n[3] Edit customer\n[4] Back\n')
                 choice = input('>>> ')
 
+            #librarian wants to add a new customer
             if choice == '1':
                 loggedIn()
                 print('\nEnter EXIT to go back (NOTE: nothing will be saved)\nPlease enter all the fields below to register a new customer: \n')
                 #all the questions to fill in
                 ask = ['Please enter your name: ', 'Please enter your surname: ', 'Please enter a username: ', 'Please enter your preferred language: ','Please enter your address: ','Please enter your zipcode: ','Please enter your city: ','Please enter your email: ','Please enter your telephone number (mobile): ','Please enter your gender ( male / female ): ']
                 
+                #print all the questions and get the input
                 data = []
                 for x in ask:
+                    #check if there is asked for gender
                     if 'gender' in x:
                         temp = input(x)
+                        #there are only 2 genders
                         while temp not in ['male','female']:
+                            #re-ask question
                             temp = input(x)
                     else:
+                        #ask question normally
                         temp = input(x)
 
+                    #check if the librarian wish to cancel
                     if temp == 'EXIT':
+                        #exit
                         return True
                     else:
+                        #add the data to the  data list
                         data.append(temp)
 
             
                 loggedIn()
+                #print all the data t4o double check
                 print('\nPlease confirm that the account below is correct: \n\nUsername: ' + data[2] + '\nFull Name: ' + data[0].title() + ' ' + data[1].title() + '\nNameSet(Language): ' + data[3].title() + '\nGender: ' + data[9].title() +'\nAddress (Combined): ' + data[6].title() + ' ' + data[4].title() + ' ' + data[5] + '\nEmail: ' + data[7] + '\nTelephonenumber: ' + data[8])
-
+                #confirm that all is as should be
                 confirm = input('\nEnter yes to save the account or no to discard and go back: ')
+                #there all only 2 answers dont stray from the path
                 while confirm not in 'yesno':
                     confirm = input('Enter yes to save the account or no to discard and go back: ')
-
+                #everything is cool so the new customer gets saved
                 if confirm == 'yes':
+                    #update the file with customers
                     self.customers.addUser(data[9], data[3].title(), data[0].title(), data[1].title(), data[4], data[5], data[6], data[7], data[2], data[8])
+                    #confirmation
                     print('\nUser has been succesfully added to the system!')
                     time.sleep(1.0)
                 else:
+                    #exit if it is no
                     return True
 
                 return True
@@ -150,7 +176,9 @@ class PLS:
             elif choice == '2':
                 while True:
                     loggedIn()
+                    #print all customers from the system
                     helper.printAllCustomers(self.customers.customers)
+                    #check which customer to yeet away (delete)
                     print('Please enter which you user you would like to remove! Enter EXIT to go back!')
                     choice = input('>>> ')
                     while True:
@@ -163,34 +191,42 @@ class PLS:
                             choice = input('Please enter a valid number or EXIT to go back >>> ')
 
                     loggedIn()
+                    #prompt if the customer has to be deleted
                     print('\nDo you wish to remove the customer: ' + self.customers.getUser(int(choice))['Username'] + '? (yes/no)')
                     temp = input('>>>')
                     while temp not in 'yesno':
                         temp = input('Please enter yes or no >>>')
 
+                    #yeet the customer away with the might of ZEUS
                     if temp == 'yes':
                         self.customers.removeUser(int(choice))
                         print('\nCustomer has been removed succesfully from the system!')
+                    #keep the customer
                     else:
                         print('Customer has not been removed from the system!')
                         time.sleep(1.0)
                 
             elif choice == '3':
                 loggedIn()
+                #print all the customers in the system
                 helper.printAllCustomers(self.customers.customers)
                 print('Please enter which you user you would like to Change!')
+                #check if the input is valid
                 choice = input('>>> ')
                 while True:
                     if choice.isdigit():
-                        if int(choice) - 1 < len(self.customers.customers):
+                        if int(choice) - 1 < len(self.customers.customers) and int(choice) > 0:
                             break
                     else:
                         choice = input('Please enter a valid number >>> ')
                 
+                #get the customer from the file and store the attributes in a list
                 names =['Gender','NameSet' ,'GivenName' ,'Surname' ,'StreetAddress' ,'ZipCode' ,'City' ,'EmailAddress' ,'Username','TelephoneNumber']
                 toEdit = self.customers.getUser(int(choice))
                 editCopy = toEdit.copy()
 
+                #when already selected a customer
+                #stay in this loop till the librarian exits
                 while True:
                     loggedIn()
                     print('What attribute would you like to change?') 
@@ -203,31 +239,46 @@ class PLS:
                     print('\n\nEnter SAVE to save the changes\nEnter EXIT to exit (Changes will not be saved if not saved!)')
                     change = input('>>> ')
                     while True:
+                        #save the changes made
                         if change == 'SAVE':
                             self.customers.editUser(int(choice), editCopy)
                             break
+                        #exit back to the customer menu going back to the main menu
                         elif change == 'EXIT':
                             return True
-
+                        #check if it;s a number representing one of the customer's attributes
                         elif change.isdigit():
                             if int(change) - 1 < len(names):
                                 break
+                        #input does not match anything
                         else:
-                            change = input('Please enter a valid number >>> ')
+                            change = input('Please enter a valid option >>> ')
+                    
+                    #check if the librarian saved
                     if change != 'SAVE':
+                        #if not then prompt the librarian to make a change
                         if names[int(change) - 1] == 'Gender':
+                            #change the gender
                             temp = input('Please enter your gender (male/female): ')
                             while temp not in ['male', 'female']:
                                 temp = input('Please enter your gender (male/female): ')
                             editCopy[names[int(change) - 1]] = temp
                         else:
+                            #change everything except gender
                             print('Change ' + names[int(change) - 1] + ': ' + toEdit[names[int(change) - 1]])
                             temp = input(names[int(change) - 1] + ': ')
                             editCopy[names[int(change) - 1]] = temp
 
             elif choice == '4':
+                #exit the menu
                 return False
             
+            #stay in this menu
+            return True
+
+        def editStaff():
+
+            #keep the loop running till the librarian exits
             return True
 
         #handeling all the options that the librarian can and cannot do
@@ -251,7 +302,9 @@ class PLS:
                     loop = customerEditing()
             #staff
             elif choice == '3':
-                pass
+                loop = True
+                while loop:
+                    loop = editStaff()
             #logging out
             elif choice == '4':
                 loggedIn()
@@ -289,7 +342,7 @@ class PLS:
                 if self.admin:
                     loop = self.librarian()
                 else:
-                    self.customer()
+                    loop = self.customer()
 
 #this is the main you know where the system knows where to run the stuff
 if __name__ == '__main__':
